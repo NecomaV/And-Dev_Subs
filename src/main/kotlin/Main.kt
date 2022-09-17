498221a36cca9c1b47aa351414ae723ede0b52a0
NecomaV <73179504+NecomaV@users.noreply.github.com>
2022-09-17 19:38:45 +0600

    3. Learn more about functions step 2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,9 +4,21 @@ fun main(args: Array<String>) {
     feedTheFish()
 
 }
+fun fishFood (day : String) : String {
+    return when (day) {
+        "Monday" -> "flakes"
+        "Wednesday" -> "redworms"
+        "Thursday" -> "granules"
+        "Friday" -> "mosquitoes"
+        "Sunday" -> "plankton"
+        else -> "nothing"
+    }
+}
+
 fun feedTheFish() {
     val day = randomDay()
-    val food = "pellets"
+    val food = fishFood(day)
+
     println ("Today is $day and the fish eat $food")
 }
 
