fe49226bfad3a714a7508211a40fbec3f6ed6400
NecomaV <73179504+NecomaV@users.noreply.github.com>
2022-09-17 13:54:25 +0600

    3. Compare conditions and booleans Write an if/else statement.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,10 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    val numberOfFish = 50
+    val numberOfPlants = 23
+    if (numberOfFish > numberOfPlants) {
+        println("Good ratio!")
+    } else {
+        println("Unhealthy ratio")
+    }
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
 }
