c428525164bc072cdfee272c6aa8e1a70a8b5642
NecomaV <73179504+NecomaV@users.noreply.github.com>
2022-09-17 19:21:10 +0600

    1. Explore the main() function

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,7 @@
 fun main(args: Array<String>) {
-    val numberOfFish = 50
-    val numberOfPlants = 23
-    if (numberOfFish > numberOfPlants) {
-        println("Good ratio!")
-    } else {
-        println("Unhealthy ratio")
-    }
+    printHello()
 
+}
+fun printHello() {
+    println ("Hello World")
 }
