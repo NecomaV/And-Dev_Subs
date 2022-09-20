120a2716c50125dee7cdb4a25601a917c7ccc4fd
NecomaV <73179504+NecomaV@users.noreply.github.com>
2022-09-20 10:56:41 +0600

    5. Get started with filters all

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,5 +3,52 @@ import java.util.*
 fun main(args: Array<String>) {
     var decorations = listOf("rock", "pagoda", "plastic plant", "alligator","flower pot")
 
-  println( decorations.filter { it[0] == 'p' })
+    var eager =  decorations.filter { it[0] == 'p' }
+    println(eager)
+
+    val filtered = decorations.asSequence().filter { it[0] == 'p' }
+    println("Filtered : $filtered")
+
+    var newList = filtered.toList()
+    println("New list: $newList")
+
+
+
+    val lazyMap = decorations.asSequence().map{
+        println("access : $it")
+        it
+    }
+    println("-----")
+    println("-----")
+    println("lazy: $lazyMap")
+    println("-----")
+    println("first: ${lazyMap.first()}")
+    println("-----")
+    println("all: ${lazyMap.toList()}")
+
+
+
+
+
+    val lazyMap2 = decorations.asSequence().filter {it[0] == 'p'}.map {
+        println("access: $it")
+        it
+    }
+    println("-----")
+    println("filtered: ${lazyMap2.toList()}")
+
+
+    val mysports = listOf("basketball", "fishing", "running")
+    val myplayers = listOf("LeBron James", "Ernest Hemingway", "Usain Bolt")
+    val mycities = listOf("Los Angeles", "Chicago", "Jamaica")
+    val mylist = listOf(mysports, myplayers, mycities)     // list of lists
+    println("-----")
+    println("Flat: ${mylist.flatten()}")
+
+
+
+
+
+
+
 }
