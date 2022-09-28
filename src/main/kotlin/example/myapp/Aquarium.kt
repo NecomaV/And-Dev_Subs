db2adfdba48ac3fc6df39415a98ca51f995bb663
NecomaV <73179504+NecomaV@users.noreply.github.com>
2022-09-29 00:32:47 +0600

    3 Path
    2. Create a class 1 2 steps

--- src/main/kotlin/example/myapp/Aquarium.kt
+++ src/main/kotlin/example/myapp/Aquarium.kt
@@ -1,9 +1,7 @@
 package example.myapp
 
-class Aquarium {
-    var width: Int = 20
-    var height: Int = 40
-    var length: Int = 100
+class Aquarium (var length: Int = 100, var width: Int = 20, var height: Int = 40){
+
 
     fun printSize() {
         println("Width: $width cm " +
@@ -11,6 +9,18 @@ class Aquarium {
                 "Height: $height cm ")
     }
 
+    init {
+        println("aquarium initializing")
+    }
+    init {
+        // 1 liter = 1000 cm^3
+        println("Volume: ${width * length * height / 1000} liters")
+    }
+    constructor(numberOfFish: Int) : this() {
+        // 2,000 cm^3 per fish + extra room so water doesn't spill
+        val tank = numberOfFish * 2000 * 1.1
+        height = (tank / (length * width)).toInt()
+    }
 
 }
 
