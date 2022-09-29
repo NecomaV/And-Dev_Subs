3218d7f644e0e829e2922e9c2f1420de3be6671d
NecomaV <73179504+NecomaV@users.noreply.github.com>
2022-09-29 10:52:31 +0600

    3 Path
    5.

--- src/main/kotlin/example/myapp/Aquarium.kt
+++ src/main/kotlin/example/myapp/Aquarium.kt
@@ -1,6 +1,8 @@
 package example.myapp
 
-open class Aquarium (var length: Int = 100, var width: Int = 20, var height: Int = 40){
+import java.lang.Math.PI
+
+open class Aquarium (var length: Int = 100, var width: Int = 20, open var height: Int = 40){
 
 
     fun printSize() {
@@ -31,4 +33,13 @@ open class Aquarium (var length: Int = 100, var width: Int = 20, var height: Int
 
 
 }
-
+class TowerTank (override var height: Int, var diameter: Int): Aquarium(height = height, width = diameter, length = diameter) {
+    override var volume: Int
+        // ellipse area = π * r1 * r2
+        get() = (width/2 * length/2 * height / 1000 * PI).toInt()
+        set(value) {
+            height = ((value * 1000 / PI) / (width/2 * length/2)).toInt()
+        }
+    override var water = volume * 0.8
+    override val shape = "cylinder"
+}
